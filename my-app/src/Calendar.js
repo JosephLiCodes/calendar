0429236bdd4d684e6b7129b9458cf47d8664bf83
JosephLiCodes <69280155+JosephLiCodes@users.noreply.github.com>
2022-01-06 18:58:48 -0800

    created calendar page

---
+++ my-app/src/Calendar.js
@@ -0,0 +1,38 @@
+import './calendar.css';
+import group1141 from './Group 1141.svg';
+import group1063 from './Group 1063.svg';
+import calendar from './CALENDAR.svg';
+import group1069 from './Group 1069.svg';
+import group1085 from './Group 1085.svg';
+import g1108 from './Group 1108.svg';
+import g1107 from './Group 1107.svg';
+import g1110 from './Group 1110.svg';
+import g1045 from './Group 1045.svg';
+function Calendar() {
+  return (
+    <div className="Calendar">
+      <link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet'></link>
+      <header className="App-header">
+      <img src={calendar} className="CALENDAR" />
+      <img src={group1141} className = "rainbow" />
+      <h1 className = "WDCText">WDC Calendar</h1>
+      <img src={group1063} className = "group1063" />
+      <img src={group1069} className = "legend" />
+      <img src={group1085} className = "group1085" />
+
+
+
+
+      <img src={g1108} className = "g1109" />
+      <img src={g1108} className = "g1108" />
+      <img src={g1107} className = "g1107" />
+      <img src={g1108} className = "g1106" />
+      <img src={g1045} className = "g1045" />
+      <img src={g1110} className = "g1110" />
+      <h2 className = "ten">10</h2>
+      </header>
+    </div>
+  );
+}
+
+export default Calendar;
